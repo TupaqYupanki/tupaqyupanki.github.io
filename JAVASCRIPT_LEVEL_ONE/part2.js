b2bab5bd9a35580a0f58927076cd2a0823f1546d
TupaqYupanki <rustarkmad@gmail.com>
2021-09-10 15:32:38 +0300

    part3.js

---
+++ JAVASCRIPT_LEVEL_ONE/part2.js
@@ -0,0 +1,4 @@
+alert("Welcome here")
+var deposit = prompt("How much would you like to deposit today?")
+alert("You've deposited: "+deposit)
+console.log("You're cool")
---
+++ JAVASCRIPT_LEVEL_ONE/part3.js
@@ -0,0 +1,4 @@
+var lbs = prompt("Enter the number")
+kg = lbs*0.454
+alert(lbs+'lbs = '+kg+'kg')
+console.log('Conversion Completed')
