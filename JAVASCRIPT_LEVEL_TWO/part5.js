1501917b8e3ba12c629adcb39f25cd542669b1fb
TupaqYupanki <rustarkmad@gmail.com>
2021-09-14 23:15:35 +0300

    JS level two proj

--- JAVASCRIPT_LEVEL_TWO/part5.js
+++ JAVASCRIPT_LEVEL_TWO/part5.js
@@ -1,6 +1,26 @@
-var carInfo = {make:'Toyota', year:1990, model:'Camry'}
+var carInfo = {make:'Toyota', year:1990, model:'Camry', carAlert: function(){alert("We've got a car here!")}}
 var myNewO = {a:'hello', b:[1,2,3], c:{inside:['a','b']}};
 for (i in carInfo){
   console.log(i);
   console.log(carInfo[i]);
+}
+var myObj = {
+  prop: 37,
+  reportProt: function() {
+    return this.prop;
+  }
+}
+
+var simple = {
+  prop: "Hello",
+  myMethod: function(){
+    console.log("The myMethod was called");
+  }
+}
+
+var myNeyObj = {
+  name: "Jose",
+  greet: function(){
+    console.log("Hello " + this.name);
+  }
 }
