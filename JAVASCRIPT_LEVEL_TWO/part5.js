f6e271e2e20c0334aafaa266d9a8fc429af86885
TupaqYupanki <rustarkmad@gmail.com>
2021-09-14 18:24:59 +0300

    JS Objects

---
+++ JAVASCRIPT_LEVEL_TWO/part5.js
@@ -0,0 +1,6 @@
+var carInfo = {make:'Toyota', year:1990, model:'Camry'}
+var myNewO = {a:'hello', b:[1,2,3], c:{inside:['a','b']}};
+for (i in carInfo){
+  console.log(i);
+  console.log(carInfo[i]);
+}
