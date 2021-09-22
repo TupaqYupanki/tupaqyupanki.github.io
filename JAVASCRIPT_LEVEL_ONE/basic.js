e232c43d0680bb370d102015ace85b0a51a62caa
TupaqYupanki <rustarkmad@gmail.com>
2021-09-22 15:59:58 +0300

    1

--- JAVASCRIPT_LEVEL_ONE/basic.js
+++ JAVASCRIPT_LEVEL_ONE/basic.js
@@ -4,7 +4,6 @@ var newelement = ''
 
 for (i in element.innerHTML){
   if (i == 0){
-    console.log(i);
     for (x in cipher)
       if (element.innerHTML[i] == cipher[x]){
         console.log(element.innerText[i]);
@@ -12,10 +11,7 @@ for (i in element.innerHTML){
       } else if (x == cipher.length-1){
         newelement = element.innerHTML[i]
       }
-
   } else if (i == element.innerHTML.length-1) {
-    console.log(i);
-    console.log(element.innerHTML.length);
     for (x in cipher)
       if (element.innerHTML[i] == cipher[x]){
         newelement = newelement+'<font color="red">'+element.innerHTML[i]+'</font>'
@@ -24,7 +20,6 @@ for (i in element.innerHTML){
         element.innerHTML = newelement
       }
   }
-
   else{
     for (x in cipher)
       if (element.innerHTML[i] == cipher[x]){
