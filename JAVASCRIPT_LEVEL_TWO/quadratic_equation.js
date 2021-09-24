37d8a5fe1f3bbb6fd9294be673c6260d4fe962c9
TupaqYupanki <rustarkmad@gmail.com>
2021-09-24 16:48:38 +0300

    2

--- JAVASCRIPT_LEVEL_TWO/quadratic_equation.js
+++ JAVASCRIPT_LEVEL_TWO/quadratic_equation.js
@@ -12,13 +12,8 @@ d = b*b - (4*a*c)
 if(d < 0){
   alert('Вещественных корней нет')
 }else{
-console.log('d = '+d);
 x1 = ((Math.sqrt(d))-b)/(2*a)
-
-console.log(typeof(x1));
 x2 = (-1*((Math.sqrt(d)))-b)/(2*a)
-console.log(x2);
-
   alert("X1 = "+x1+'\n'+"X2 = "+x2)
 }
 }
