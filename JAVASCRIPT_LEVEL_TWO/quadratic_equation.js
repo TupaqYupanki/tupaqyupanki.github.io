bbc7444b04e1260ff02a50d1ea2ebef78556b7a7
TupaqYupanki <rustarkmad@gmail.com>
2021-09-24 17:20:52 +0300

    3

--- JAVASCRIPT_LEVEL_TWO/quadratic_equation.js
+++ JAVASCRIPT_LEVEL_TWO/quadratic_equation.js
@@ -4,14 +4,24 @@ var c = prompt("Введите значение C")
 
 function square(a,b,c){
 
+  if(parseInt(a)==NaN || parseInt(b)==NaN || parseInt(c)==NaN){
+  // console.log(typeof(a));
+  // console.log(typeof(b));
+  // console.log(typeof(c));
+  console.log('Введите число');
+  }
+
 a = Number(a)
 b = Number(b)
 c = Number(c)
+
+
 d = b*b - (4*a*c)
 
-if(d < 0){
+if((d < 0) || (a == 0)){
   alert('Вещественных корней нет')
-}else{
+}else
+{
 x1 = ((Math.sqrt(d))-b)/(2*a)
 x2 = (-1*((Math.sqrt(d)))-b)/(2*a)
   alert("X1 = "+x1+'\n'+"X2 = "+x2)
