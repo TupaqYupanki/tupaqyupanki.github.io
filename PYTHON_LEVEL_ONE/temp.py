8c2ce874cd887952645975760eb01698443df31a
TupaqYupanki <rustarkmad@gmail.com>
2021-12-08 14:01:02 +0300

    part 8 function

--- PYTHON_LEVEL_ONE/temp.py
+++ PYTHON_LEVEL_ONE/temp.py
@@ -6,3 +6,26 @@ def my_func(param1='default'):
 
 
 my_func()
+
+def addNum(num1,num2):
+    if type(num1) == type(num2)==type(10):
+        return num1+num2
+    else:
+        return "Sorry, I need integers!"
+
+result = addNum('2',3)
+print(result)
+
+# Filter
+mylist = [1,2,3,4,5,6,7,8]
+def even_bool(num):
+    return num%2 == 0
+
+evens = filter(even_bool,mylist)
+print (evens)
+print(list(evens))
+
+tweet = 'Go Sports! #Sports'
+print (tweet.split('#'))
+
+print('x' in [1,2,3])
